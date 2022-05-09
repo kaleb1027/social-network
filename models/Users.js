4e521fb056d19f1fe5525b45c13f2b1b97813894
Kaleb Sherrill <kaleb1027@gmail.com>
2022-05-09 01:47:50 -0400

    Finished models

---
+++ models/Users.js
@@ -0,0 +1,48 @@
+const { Schema, model } = require('mongoose');
+
+var validEmail = function(email) {
+    var re = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+    return re.test(email)
+};
+const UsersSchema = new Schema(
+    {
+    username: {
+        type: String,
+        unique: true,
+        required: true,
+        trim: true
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+        validate: [validEmail, 'Please fill in a valid email address'],
+    },
+    thoughts: [{
+        type: Schema.Types.ObjectId,
+        ref: 'Thoughts'
+    }],
+    friends: [{
+        type: Schema.Types.ObjectId,
+        ref: 'Users'
+    }]
+    },
+    {
+    toJSON: {
+        virtuals: true,
+        getters: true,
+    },
+    id: false
+    }
+)
+
+
+UsersSchema.virtual('friendCount').get(function() {
+    return this.friends.length;
+})
+
+
+const Users = model('Users', UsersSchema);
+
+
+module.exports = Users;
